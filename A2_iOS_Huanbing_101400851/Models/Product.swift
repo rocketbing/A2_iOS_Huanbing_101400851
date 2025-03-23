7bdcdfce9889857f10c693e647b9fcd4fc8ec508
Steve Zhang <156036235+rocketbing@users.noreply.github.com>
2025-03-23 14:40:01 -0400

    Product Model variables added

--- A2_iOS_Huanbing_101400851/Models/Product.swift
+++ A2_iOS_Huanbing_101400851/Models/Product.swift
@@ -7,6 +7,14 @@ public class Product: NSManagedObject {
 }
 
 extension Product {
-    
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Product> {
+        return NSFetchRequest<Product>(entityName: "Product")
+    }
+
+    @NSManaged public var productId: Int64
+    @NSManaged public var productName: String?
+    @NSManaged public var productDescription: String?
+    @NSManaged public var productPrice: Double
+    @NSManaged public var productProvider: String?
 }
 
