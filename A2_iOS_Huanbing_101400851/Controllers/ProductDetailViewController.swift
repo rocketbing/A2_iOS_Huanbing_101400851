5173eeb5abf56099cc0a4a8a9bd5a5f0d8e02b8c
Steve Zhang <156036235+rocketbing@users.noreply.github.com>
2025-03-23 14:44:39 -0400

    Product Detail Controller created

---
+++ A2_iOS_Huanbing_101400851/Controllers/ProductDetailViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class ProductDetailViewController: UIViewController {
+
+    var product: Product?
+    
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productDescriptionLabel: UILabel!
+    @IBOutlet weak var productPriceLabel: UILabel!
+    @IBOutlet weak var productProviderLabel: UILabel!
+    
+    
+}
+
