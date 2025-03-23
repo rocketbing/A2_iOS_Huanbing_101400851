c30a0a3de8c93ae4a1f70fd618809bee48da67b3
Steve Zhang <156036235+rocketbing@users.noreply.github.com>
2025-03-23 14:43:06 -0400

    two more functions added

--- A2_iOS_Huanbing_101400851/Controllers/ProductListViewController.swift
+++ A2_iOS_Huanbing_101400851/Controllers/ProductListViewController.swift
@@ -5,7 +5,7 @@ class ProductListViewController: UITableViewController {
     
     var products = [Product]()
     var context: NSManagedObjectContext!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
@@ -22,4 +22,18 @@ class ProductListViewController: UITableViewController {
         }
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return products.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell", for: indexPath)
+        let product = products[indexPath.row]
+        cell.textLabel?.text = product.productName
+        cell.detailTextLabel?.text = product.productDescription
+        return cell
+    }
+    
+    
 }
+
