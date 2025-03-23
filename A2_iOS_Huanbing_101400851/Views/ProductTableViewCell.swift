d3d89062165fe27bb29a372d1051f92b75aeed02
Steve Zhang <156036235+rocketbing@users.noreply.github.com>
2025-03-23 14:51:53 -0400

    ProductTaleViewCell updated

--- A2_iOS_Huanbing_101400851/Views/ProductTableViewCell.swift
+++ A2_iOS_Huanbing_101400851/Views/ProductTableViewCell.swift
@@ -1,7 +1,24 @@
-//
-//  ProductTableViewCell.swift
-//  A2_iOS_Huanbing_101400851
-//
-//  Created by Steve Zhang on 2025-03-23.
-//
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+    
+    // UI elements for product name and description
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productDescriptionLabel: UILabel!
+    
+    // Configure the cell with product information
+    func configureCell(with product: Product) {
+        productNameLabel.text = product.productName
+        productDescriptionLabel.text = product.productDescription
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Custom setup for the cell if needed
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
 
