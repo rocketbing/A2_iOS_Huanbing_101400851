196e95ef6a806444c98008478d904f446275e100
Steve Zhang <156036235+rocketbing@users.noreply.github.com>
2025-03-23 14:45:03 -0400

    function added in product detail controller

--- A2_iOS_Huanbing_101400851/Controllers/ProductDetailViewController.swift
+++ A2_iOS_Huanbing_101400851/Controllers/ProductDetailViewController.swift
@@ -9,6 +9,14 @@ class ProductDetailViewController: UIViewController {
     @IBOutlet weak var productPriceLabel: UILabel!
     @IBOutlet weak var productProviderLabel: UILabel!
     
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let product = product {
+            productNameLabel.text = product.productName
+            productDescriptionLabel.text = product.productDescription
+            productPriceLabel.text = "$\(product.productPrice)"
+            productProviderLabel.text = product.productProvider
+        }
+    }
 }
 
