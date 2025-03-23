cb0b27d007034f8a7dd6b150899ff6ff835c286e
Steve Zhang <156036235+rocketbing@users.noreply.github.com>
2025-03-23 14:46:13 -0400

    variables and function added

--- A2_iOS_Huanbing_101400851/Controllers/AddProductViewController.swift
+++ A2_iOS_Huanbing_101400851/Controllers/AddProductViewController.swift
@@ -3,7 +3,17 @@ import CoreData
 
 class AddProductViewController: UIViewController {
 
-  
+    @IBOutlet weak var productNameTextField: UITextField!
+    @IBOutlet weak var productDescriptionTextField: UITextField!
+    @IBOutlet weak var productPriceTextField: UITextField!
+    @IBOutlet weak var productProviderTextField: UITextField!
+    
+    var context: NSManagedObjectContext!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    }
     
     
 }
